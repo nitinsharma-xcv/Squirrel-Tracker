f6fc8ae4dade567fa46f43515f60e306e0b4d041
Niharika Prasad <niharikap24@gmail.com>
2021-03-18 04:27:42 -0400

    Export functionality implemented

--- squirrel_tracker/squirrel_tracker_app/management/commands/export_squirrel_data.py
+++ squirrel_tracker/squirrel_tracker_app/management/commands/export_squirrel_data.py
@@ -3,7 +3,6 @@
 # Author(s): Niharika Prasad (np2781) <niharika.prasad@columbia.edu>
 
 from django.core.management.base import BaseCommand, CommandError
-import csv
 from ...models import Squirrel
 import pandas as pd
 
@@ -15,19 +14,20 @@ class Command(BaseCommand):
         parser.add_argument('file_path', type=str)
 
     def handle(self, *args, **options):
-
         try:
             file_path = options['file_path']
-            vals = Squirrel.objects.all().values()
-            df = pd.DataFrame(list(vals))
-            df.columns = [c.replace('_', ' ').title() for c in df.columns]
-            for bool_col in df.dtypes[df.dtypes == bool].index:
-                df[bool_col] = df[bool_col].astype(str).str.upper()
-            df['Date'] = df['Date'].apply(lambda d: d.strftime('%m%d%Y'))
-            df.to_csv(file_path, index=False)
+            values = Squirrel.objects.all().values()
+            df = pd.DataFrame(list(values))
+            df.columns = [c.verbose_name for c in Squirrel._meta.get_fields()]
 
+            for bool_col in df.dtypes[df.dtypes == bool].index:
+                df[bool_col] = df[bool_col].astype(str).str.lower()
+
+            df['Date'] = df['Date'].apply(lambda d: d.strftime('%m%d%Y'))
+            df['Lat/Long'] = [obj.lat_long for obj in Squirrel.objects.all()]
+
+            df.to_csv(file_path, index=False)
         except Exception as e:
             raise CommandError('Error', e)
 
-        # self.stdout.write(self.style.SUCCESS(f'Successfully Imported {file_path}'))
-        self.stdout.write(self.style.SUCCESS(f'Functionality to be implemented'))
+        self.stdout.write(self.style.SUCCESS(f'Data Successfully Exported to {file_path}'))
