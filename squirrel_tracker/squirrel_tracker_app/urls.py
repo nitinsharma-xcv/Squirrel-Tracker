d6c5256dd86ac1bb58602533914c16f97b9aa6a5
Niharika Prasad <niharikap24@gmail.com>
2021-04-11 20:24:16 -0400

    Implement list sightings view

--- squirrel_tracker/squirrel_tracker_app/urls.py
+++ squirrel_tracker/squirrel_tracker_app/urls.py
@@ -1,6 +1,7 @@
 # urls.py
 # Created on Mar 04, 2021 
-# Author(s): Nitin Sharma (ns3493) <nitin.sharma@columbia.edu>
+# Author(s): Nitin Sharma (ns3493) <nitin.sharma@columbia.edu>,
+#            Niharika Prasad (np2781) <niharika.prasad@columbia.edu>
 
 from django.urls import path
 
@@ -8,4 +9,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('sightings/', views.list_of_squirrels, name='list_of_squirrels'),
 ]
--- squirrel_tracker/squirrel_tracker_app/views.py
+++ squirrel_tracker/squirrel_tracker_app/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Squirrel
 
 
 def index(request):
     return HttpResponse("Hello, world!")
+
+
+def list_of_squirrels(request):
+    list_squirrels = Squirrel.objects.all()
+    context = {'squirrels': list_squirrels}
+    return render(request, 'squirrel_tracker_app/list_all_squirrels.html', context)
