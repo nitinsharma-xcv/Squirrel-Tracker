4f45b48b2e7717da3e326afeffece16358733799
nitinsharma-xcv <nitinsharma.xcv@gmail.com>
2021-04-15 05:05:39 -0400

    Basic clean-up

--- squirrel_tracker/squirrel_tracker/settings.py
+++ squirrel_tracker/squirrel_tracker/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY') or 'abcd1234'
+SECRET_KEY = os.environ.get('SECRET_KEY') or 'sbdfyu4tb4hu5y2342q5qtnj4i3qtjbgeh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG') or '').strip().lower() in ('1', 'true')
